e77fe696302f48b02db3c62526e9ab5ea45bc82b
Enrico Robazza <enricorobazzi@gmail.com>
2020-06-22 12:45:43 -0300

    Included django heroku

--- invest_balance/settings.py
+++ invest_balance/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'invest_balance/static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'invest_balance/static/')]
+
+django_heroku.settings(locals())
