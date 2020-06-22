38abd6b4632a39b499746089d4fa6397510ff57f
Enrico Robazza <enricorobazzi@gmail.com>
2020-06-22 11:20:38 -0300

    Deploy config

--- invest_balance/settings.py
+++ invest_balance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0fuo#l8e!qkm^y6-40ln@(rh7126v0o#^ewz%$sigf5nk)ihnp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
