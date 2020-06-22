789bf5de54a4c3792d5546c44ea402cf0a7993ce
Enrico Robazza <enricorobazzi@gmail.com>
2020-06-22 12:47:20 -0300

    updated static files dir

--- invest_balance/settings.py
+++ invest_balance/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'invest_balance/static')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'invest_balance/static/')]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
